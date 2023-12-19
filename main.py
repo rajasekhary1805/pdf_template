22fa6cfa46c65799c6ed61d2a51498cb0d4a78d0
Rajasekhar <yrajasekhar309@gmail.com>
2023-12-19 17:24:32 +0530

    Add Lines to all pages

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fpdf import FPDF
 import pandas as pd
 
+
+def ln_fn():
+    # Set the Lines
+    for i in range(20, 290, 10):
+        pdf.line(10, i, 200, i)
+
+    # Set the footer
+    pdf.ln(270)
+    pdf.set_font(family='Helvetica', size=10)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=12, txt=row['Topic'], align='R')
+
+
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 pdf.set_auto_page_break(auto=False, margin=0)
 
@@ -12,22 +25,10 @@ for index, row in df.iterrows():
     pdf.set_font(family='Helvetica', size=24)
     pdf.set_text_color(0, 0, 0)
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L')
-    pdf.line(10, 21, 200, 21)
-
-    # Set the footer
-    pdf.ln(270)
-    pdf.set_font(family='Helvetica', size=10)
-    pdf.set_text_color(180, 180, 180)
-    pdf.cell(w=0, h=12, txt=row['Topic'], align='R')
+    ln_fn()
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
-
-        # Set the footer
-        pdf.ln(270)
-        pdf.set_font(family='Helvetica', size=10)
-        pdf.set_text_color(180, 180, 180)
-        pdf.cell(w=0, h=12, txt=row['Topic'], align='R')
-
+        ln_fn()
 
 pdf.output('output.pdf')
